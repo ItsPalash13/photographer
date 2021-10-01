436d3345a5ac5967b423294689560e7652f20cbd
Palash Vishwas <84432951+itspalash@users.noreply.github.com>
2021-10-01 12:26:43 +0530

    Add files via upload

---
+++ index1.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Photographers</title>
+  <link rel="icon" href="images/logo1.svg" type="image/svg" sizes="16x16">
+  <link rel="stylesheet" href="index.css">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Italiana&family=Poppins:wght@300&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link href="https://fonts.googleapis.com/css2?family=Oswald:wght@500&&display=swap" rel="stylesheet">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://kit.fontawesome.com/ff9c3085d9.js" crossorigin="anonymous"></script>
+<link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
+</head>
+<style type="text/css">
+#a{position:absolute; top:63%; left:45%; width:155px; height:155px; border-radius:77.5px; background:rgb(28,28,28);transition: transform .2s;}
+
+  #a:hover{
+    transform: scale(1.1); 
+}
+
+</style>
+<body style="overflow-x:hidden;">
+
+<div style="position:absolute; left:0%; top:0%; width:100%; height:200%;">
+  <img src="images/s1.svg" style="position:absolute; top:-11%; height:70%; width:50%; left:-25%; transform: rotate(10deg)">
+  <img src="images/s2.svg" style="position:absolute; top:-3%; height:15%; left:70%; transform: rotate(-10deg);">
+  <p class="inner" style="position:relative; left:15.3%; top:-7.7%; font-size:190px;font-family: 'Oswald', sans-serif; color:rgb(28,28,28);">PALASH <span style="opacity:0; color:#e6e6e6; opacity:0.05;">KRISHNA</span></p>
+
+     <p style="position:fixed; z-index:5; left:46%; top:-4%; font-size:25px;font-family: 'Oswald', sans-serif; color:black;">PALASH V.</p>
+
+    <p style="position:absolute; left:7%; top:23%; font-size:40px;font-family: 'Oswald', sans-serif; color:#f4cd08;"><span>I SHOW REALITY</span><br><span style="position:relative; top:-20px;">FROM IT’S</span><br><span style="position:relative; top:-40px;">BETTER SIDE</span></p>
+
+     <p style="z-index:5; position:fixed; left:1.4%; top:-1%; font-size:18px;font-family: 'Oswald', sans-serif; color:black;"><span style="color:#383838;">HOME</span><br><br><span style="position:relative; top:-14px;">SERVICES</span><br><br><span style="position:relative; top:-28px;">WORKS <i class="fa fa-caret-down"></i></span></p>
+
+     <p style="position:fixed; right:1.7%; top:0.5%; font-size:20px;font-family: 'Oswald', sans-serif; color:black; z-index:5;"><span style="color:black;">+91 9049-59691</span></p>
+    
+    <p style="position:absolute; right:12%; top:14%; font-size:40px;font-family: 'Oswald', sans-serif; color:rgb(244,205,8);"><span>BASED</span><br><span style="position:relative; top:-20px;">IN INDIA</span><img src="images/flag.png" style="border-radius:2px; width:30px; transform: rotate(7deg); position:relative; top:-60px; left:-10px;"></p>
+
+  <p class="inner1" style="position:relative; left:30%; top:-30.5%; font-size:190px;font-family: 'Oswald', sans-serif; color:rgb(28,28,28);">VISHWAS</p>
+  <p style="position:relative; left:6.3%; top:-52%; font-size:190px;font-family: 'Oswald', sans-serif; -webkit-text-fill-color: white; /* Will override color (regardless of order) */
+  -webkit-text-stroke-width: 2px;
+  -webkit-text-stroke-color: black;">PHOTOGRAPHER</p>
+<img src="images/i1.jpg" style="position:absolute; top:39.8%; left:51.9%; width:45%; z-index:-2; clip-path: polygon(0 0, 100% 0, 100% 67%, 0 67%);">
+
+<svg style="position:absolute; top:83%; left:70%; width:700px; height:700px;" viewBox="0 0 700 700" shape-rendering="geometricPrecision" text-rendering="geometricPrecision">
+
+   <path id="emBlvpb1xtV2" d="M173.138000,129.477000C178.433000,125.757000,252.885000,68.464400,275.462000,66.255800C291.631000,64.674100,276.667000,99.944100,269.791000,114.663000C244.101000,169.652000,210.384000,222.158000,180.714000,275.046000C157.140000,317.067000,127.495000,360.062000,110.185000,405.351000C109.292000,407.689000,107.922000,412.742000,110.422000,412.856000C118.340000,413.218000,138.063000,389.533000,140.005000,387.573000C222.691000,304.108000,308.574000,223.194000,387.654000,136.277000C396.861000,126.158000,419.284000,92.775900,415.159000,105.819000C406.811000,132.214000,393.243000,158.493000,381.052000,183.185000C373.700000,198.076000,280.594000,366.108000,278.734000,364.481000C272.138000,358.710000,294.832000,323.311000,295.991000,321.344000C324.961000,272.190000,363.777000,230.231000,407.866000,194.276000C418.426000,185.663000,454.037000,151.119000,471.259000,154.786000C484.438000,157.593000,468.911000,208.192000,467.847000,212.241000C458.032000,249.576000,442.988000,284.702000,426.938000,319.685000C423.646000,326.861000,407.508000,351.132000,414.799000,360.927000C423.184000,372.192000,428.515000,375.512000,429.217000,392.796000C430.211000,417.256000,425.285000,442.031000,419.807000,465.724000C415.760000,483.231000,408.014000,501.952000,408.296000,520.216000C408.621000,541.245000,444.711000,489.663000,445.766000,488.090000C458.403000,469.245000,470.097000,449.339000,484.558000,431.784000C497.035000,416.638000,497.182000,443.423000,497.080000,450.604000C496.296000,505.319000,481.316000,559.108000,482.163000,613.959000C482.408000,629.820000,489.247000,625.309000,497.690000,615.657000C514.564000,596.369000,526.925000,572.825000,546.419000,555.868000C578.720000,527.769000,623.589000,572.654000,646.321000,594.592000C659.058000,606.883000,675.077000,619.994000,683.413000,636.041000" transform="matrix(1 0 0 1 -106.21796698000003 -61.12369137500002)" fill="none" stroke="rgb(0,0,0)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" stroke-dashoffset="0" stroke-dasharray="2530.800000"></path>
+  Sorry, your browser does not support inline SVG.
+</svg>
+  <div id="a">
+    <center><p style="position:relative; margin-top:38%; font-size:23px;font-family: 'Oswald', sans-serif; color:white">WRITE ME</p></center>
+  </div>
+ 
+<p style="position:absolute; left:19%; top:57%; font-size:160px;font-family: 'Oswald', sans-serif; color:rgb(28,28,28);">15 
+<span style="position:relative; top:-50px; left:-15px;">
+<span style="font-size:40px;">YEARS OF</span><br>
+<span style="float:left; margin-left:180px; font-size:40px; position:relative; top:-45px;">EXPERIENCE</span>
+</span>
+</p>
+
+<p style="position:absolute; left:7%; top:78%; font-size:160px;font-family: 'Oswald', sans-serif; color:rgb(28,28,28);">27 
+<span style="position:relative; top:-50px; left:-15px;">
+<span style="font-size:40px;">COUNTRIES</span><br>
+<span style="float:left; margin-left:186px; font-size:40px; position:relative; top:-45px;">PHOTOGRAPHED</span>
+</span>
+</p>
+
+<p style="position:absolute; left:35%; top:99%; font-size:160px;font-family: 'Oswald', sans-serif; color:rgb(28,28,28);">1 000 000 
+<span style="position:relative; top:-50px; left:-15px;">
+<span style="font-size:40px;">FINISHED</span><br>
+<span style="float:left; margin-left:82.7%; font-size:40px; position:relative; top:-45px;">SHOTS</span>
+</span>
+</p>
+
+</div>
+
+<div style="position:absolute; left:0%; top:260%; width:100%; height:50%;">
+
+    <img src="images/s1.svg" style="position:relative; top:0%; height:700%; left:0%;">
+</div>
+
+<script type="text/javascript">
+  var offset = $(".inner").offset();
+  var offset1 = $(".inner1").offset();
+$(window).scroll(function(event) {
+  var st = $(this).scrollTop();
+  $(".inner").css("left",  offset.left - st);
+  $(".inner1").css("left",  offset1.left + st);
+});
+
+</script>
+</body> 
+</html>
